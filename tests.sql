40bb2c1bec046ae388a726fd985ccde63b7fd7b4
Fernando Vilar Martinho <76790473+fernandovilar@users.noreply.github.com>
2022-02-27 18:29:33 -0300

    more tests

--- tests.sql
+++ tests.sql
@@ -82,5 +82,52 @@ select * from test5 where lower(user_name) like '%joao%' -- 2 records, whitout a
 select * from test5 where unaccent(user_name) like '%joao%' -- 1 record
 select * from test5 where unaccent(lower(user_name)) like '%joao%' -- 5 records !!! 
 
+-- 
+-- REF https://www.postgresql.org/docs/current/collation.html#COLLATION-NONDETERMINISTIC
+
+CREATE COLLATION case_insensitive (
+  provider = icu,
+  locale = 'und-u-ks-level2',
+  deterministic = false
+);
+
+CREATE COLLATION ignore_accents (
+	provider = icu, 
+	locale = 'und-u-ks-level1-kc-true', 
+	deterministic = false
+);
+
+CREATE TABLE test6 (
+	user_name varchar(100) collate ignore_accents
+);
+
+insert into test6 values ('João'), ('JOÃO'), ('JOão'), ('Joao'), ('joao')
+
+select * from test6 where user_name = 'joao' -- only 1
+select * from test6 where user_name like '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
+select * from test6 where user_name ilike '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
+select * from test6 where lower(user_name) = 'joao' -- 5 records
+select * from test6 where lower(user_name) like '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
 
 
+--
+-- REF https://postgresql.verite.pro/blog/2019/10/14/nondeterministic-collations.html
+drop table test7
+drop collation case_insensitive_ignore_accents
+CREATE COLLATION case_insensitive_ignore_accents (
+  provider = 'icu',
+  locale = '@colStrength=primary', -- or 'und-u-ks-level1'
+  deterministic = false
+);
+
+CREATE TABLE test7 (
+	user_name varchar(100) collate case_insensitive_ignore_accents
+);
+
+insert into test7 values ('João'), ('JOÃO'), ('JOão'), ('Joao'), ('joao')
+
+select * from test7 where user_name = 'joao' -- 5 
+select * from test7 where user_name like '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
+select * from test7 where user_name ilike '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
+select * from test7 where lower(user_name) = 'joao' -- 5 records
+select * from test7 where lower(user_name) like '%joao%' -- ERROR:  nondeterministic collations are not supported for LIKE - SQL state: 0A000
